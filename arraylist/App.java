07b6240e6a15a382794dff5ed57a7156e3b7b548
prafullakumarsahu <prafullakumarsahu001@gmail.com>
2020-10-02 03:59:55 +0530

    Basic operations in ArrayLists

--- arraylist/App.java
+++ arraylist/App.java
@@ -4,6 +4,9 @@
 package collections.arraylist;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -59,11 +62,53 @@ public class App {
 		//Using List interface
 		List<String> list = new ArrayList<String>();
 		list.add("Animals");
-		list.add("Birds");
 		list.add("trees");
+		list.add("Birds");
 		//Traversing through each element list style
 		for (String i: list) {
 			System.out.println(i);
+		}
+		
+		/* Sorting the list */
+		//Natural sorting
+		Collections.sort(list);
+		System.out.println(list);
+		
+		//custom sorting
+		list.sort((o1, o2) -> {return (o1.hashCode()-o2.hashCode()); });
+		System.out.println(list);
+		
+		//ArrayList to list conversion
+		List<String> letters = new ArrayList<String>();
+
+		// add example
+		letters.add("A");
+		letters.add("B");
+		letters.add("C");
+		
+		//convert list to array
+		String[] strArray = new String[letters.size()];
+		strArray = letters.toArray(strArray);
+		System.out.println(Arrays.toString(strArray)); // "[A, B, C]"
+		
+		//convert array to list
+		String[] vowels = {"a", "e", "i", "o", "u" };
+		List<String> vowelsList = Arrays.asList(vowels);
+		System.out.println(vowelsList);
+		
+		//Traversing through each element of string list
+		for (String i: vowelsList) {
+			System.out.println(i);
+		}
+		
+		//Appending one list to another
+		letters.addAll(vowelsList);
+		System.out.println(letters);
+		
+		//Iterating using while
+		Iterator lettersIterator = letters.iterator();
+		while(lettersIterator.hasNext()) {
+			System.out.println(lettersIterator.next());
 		}
 	}
 
